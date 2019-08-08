756a700969253757e4b3e00f899ca10e7d239187
HJQ <v-huangjiaqian@midland.local>
2019-08-08 11:17:44 +0800

    提交

--- KotlinMall/module_provider/src/main/java/com/hjq/kotlin/provider/interceptor/LoginInterceptorImpl.kt
+++ KotlinMall/module_provider/src/main/java/com/hjq/kotlin/provider/interceptor/LoginInterceptorImpl.kt
@@ -12,6 +12,9 @@ import com.hjq.kotlin.provider.router.RouterPath
 
 @Interceptor(name = "login", priority = 6)
 class LoginInterceptorImpl: IInterceptor {
+
+    private lateinit var context: Context
+
     override fun process(postcard: Postcard, callback: InterceptorCallback?) {
         var path = postcard.path;
         var islogin = AppPrefsUtils.getString(BaseConstant.KEY_SP_TOKEN).isNotEmpty()
@@ -27,9 +30,29 @@ class LoginInterceptorImpl: IInterceptor {
                 }
             }
         }
+
+        /*
+        //给需要跳转的页面添加值为Constant.LOGIN_NEEDED的extra参数，用来标记是否需要用户先登录才可以访问该页面
+        //先判断需不需要
+        if(postcard.getExtra() == Constant.LOGIN_NEEDED){
+
+            boolean isLogin = App.getSharedPreferences().getBoolean(Constant.IS_LOGIN,false);
+            Log.i(TAG, "是否已登录: " + isLogin);
+
+            //判断用户的登录情况，可以把值保存在sp中
+            if (isLogin) {
+                callback.onContinue(postcard);
+            }else {//没有登录,注意需要传入context
+                ARouter.getInstance().build(RouterPath.LOGIN_ACTIIVTY).navigation(mContext);
+            }
+        } else {//没有extra参数时则继续执行，不做拦截
+            callback.onContinue(postcard);
+        }
+        */
     }
 
-    override fun init(context: Context?) {
+    override fun init(context: Context) {
+        this.context = context
         Log.d("message","路由登录拦截器初始化成功"); //只会走一次
     }
 
